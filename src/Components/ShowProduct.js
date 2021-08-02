6fea0526eb7e9b85179693ebeaf7b7bc7c44403b
kentankengboon <kentankengboon@gmail.com>
2021-08-02 16:18:47 +0800

    code

--- src/Components/ShowProduct.js
+++ src/Components/ShowProduct.js
@@ -5,6 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Card} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 //let todayStamp = new Date();
+let poExisted = "";
+
 
 class ShowProduct extends React.Component{
     constructor(props){
@@ -13,7 +15,8 @@ class ShowProduct extends React.Component{
             product: [],
             pictures: [],
             message: [],
-            key: ''
+            key: '',
+            //keyMsg: ''
         }
     }
     async componentDidMount(){
@@ -46,13 +49,26 @@ class ShowProduct extends React.Component{
             if (docMsg.exists){
                 this.setState({
                     message: docMsg.data(),
-                    key: docMsg.id,
+                    //keyMsg: docMsg.id,
                     isLoading: false
                 })
             }else{console.log("No such message")}
           });
           
 
+          await firebase.storage().ref(`${this.state.product.stallId}`).child("PO.pdf")
+          .getDownloadURL().then(urlPO=>{
+            if (urlPO != null){this.setState({urlPO})}
+              console.log("URL-PO existed:   " + urlPO);
+          })
+          .catch((err) => {
+            // show Default userPic    
+            console.log("err >> ", err);
+          });
+
+          if (this.state.urlPO != null){poExisted = "PO existed. Click Open to view"}else{poExisted ="Pending PO upload"}
+          console.log("when start: " + poExisted);
+          this.setState({poExisted});
     }
     delete(id){ // can we delete the whole folder iunder email+since folder?
 
@@ -87,54 +103,60 @@ class ShowProduct extends React.Component{
     }
 
     handleChanges = async (e) => {
-        
         if(await e.target.files[0]){this.setState({image: e.target.files[0]})}
         var todayStamp = new Date();
-        console.log("herrrr at today")
-        console.log("new time :   " + todayStamp)
         const uploadTask = firebase.storage().ref(`${this.state.product.stallId}/${this.state.product.whouploadId}/${this.state.product.stallId + todayStamp}`).put(this.state.image)
         uploadTask.on('state_changed', (snapshot)=>{console.log('snapshot ok')},
         (error) =>{console.log(error);},
-        async ()=>{await firebase.storage().ref(`${this.state.product.stallId}/${this.state.product.whouploadId}`).child(this.state.product.stallId + todayStamp).getDownloadURL().then(url=>{
-            
-            this.setState({url});
-            //console.log("here first:   " + url);
-        
-        /////////// todo: to continue to update firebase data. see if below works
-        //    console.log("Url: " + url);
-        })
+        async ()=>{ await firebase.storage().ref(`${this.state.product.stallId}/${this.state.product.whouploadId}`).child(this.state.product.stallId + todayStamp)
+            .getDownloadURL().then(url=>{
+                this.setState({url});
+                //console.log("here first:   " + url);
+            })
     
-        //console.log("key:   " + this.state.key)
-        //console.log("url:  " + this.state.url)
-        firebase.firestore().collection("req@gmail.com").doc(this.state.key).collection("pictures").add({
-           image: this.state.url}). then((docRef2)=>{
-           this.setState({image: this.state.url});          
-           //this.props.history.push("/list")
-        })
+            //console.log("key:   " + this.state.key)
+            //console.log("url:  " + this.state.url)
+            firebase.firestore().collection("req@gmail.com").doc(this.state.key).collection("pictures").add({
+            image: this.state.url}). then((docRef2)=>{
+            this.setState({image: this.state.url});
+            })
 
-        const pictures=[];
-        var snapshotPic = await firebase.firestore().collection("req@gmail.com").doc(this.state.key).collection("pictures").get();
-        snapshotPic.forEach(docPic => {
-            const {image} = docPic.data();
-            pictures.push({
-              key: docPic.id, docPic, image
+            const pictures=[];
+            var snapshotPic = await firebase.firestore().collection("req@gmail.com").doc(this.state.key).collection("pictures").get();
+            snapshotPic.forEach(docPic => {
+                const {image} =docPic.data();
+                pictures.push({
+                key: docPic.id, docPic, image
+                });
             });
-          });
-          this.setState({pictures});
+            this.setState({pictures});
+        })
+    }
 
-    })
-        //const uploadTask1 = firebase.storage().ref(`${stallIdNo}/${emailUser}/${stallIdNo}_1`).put(this.state.image)
-        //uploadTask1.on('state_changed', (snapshot)=>{console.log('snapshot_1 ok')},
-        //(error) =>{console.log(error);},
-    
-        //()=>{firebase.storage().ref(`${stallIdNo}/${emailUser}`).child(stallIdNo + "_1").getDownloadURL().then(url1=>{this.setState({url1})})})  
- 
-        //console.log(e.target.files[0]);
-        
-       
+    uploadPO = async (e) => {
+        if(await e.target.files[0]){this.setState({filePO: e.target.files[0]})}
+        //const uploadTask2 = firebase.storage().ref(`${this.state.product.stallId}/${this.state.product.whouploadId}/PO`).put(this.state.filePO)
+        const uploadTask2 = firebase.storage().ref(`${this.state.product.stallId}/PO.pdf`).put(this.state.filePO)
+        uploadTask2.on('state_changed', (snapshot)=>{console.log('snapshot ok')},
+        (error) =>{console.log(error);},
+        async ()=>{ await firebase.storage().ref(`${this.state.product.stallId}`).child("PO.pdf")
+            .getDownloadURL().then(urlPO=>{
+                this.setState({urlPO});
+                console.log("URL PO:   " + urlPO);
+                poExisted = "PO existed. Click Open to view"
+                this.setState({poExisted});
+            })
+
+            // this below write the PO URL to firestore
+            firebase.firestore().collection("req@gmail.com").doc(this.state.key).update({
+            poUploaded: this.state.urlPO}) 
+
+        })
+
         
 
     }
+
 
     render (){
         const cardStyles = {
@@ -151,82 +173,93 @@ class ShowProduct extends React.Component{
             paddingRight: '0px',
             borderStyle: 'outset',
             borderLeft: '20px solid green',
-    
             //borderRadius: '20px'
-            }
+        }
+
         return(
             <div>
-                
-  <div class="column">
-    <div class="card">
-        <card style ={cardStyles}>
-                    <div className = "Button">
-                        <Link to ="/list"> 
-                        <button class ="Edit-Button" >List items</button>
-                        </Link>
+                <div class="column">
+                    <div class="card">
+                        <card style ={cardStyles}>
+                            <div className = "Button">
+                                <Link to ="/list"> 
+                                <button class ="Edit-Button" >List items</button>
+                                </Link>
+                            </div>
+                            &nbsp;
+                            <div>
+                                <img src = {this.state.product.image} height = "200" width = "200"/>
+                            </div>
+                            <div class= "panel panel-default">
+                                <h3 class="panel-title">{this.state.product.whatPN}</h3>
+                            </div>
+                            <div class = "panel-body">
+                                <dl>
+                                    <dd>Qty:  {this.state.product.whatQty}</dd>
+                                </dl>
+                                <dl>
+                                    <dt>Model</dt>
+                                    <dd>{this.state.product.whatModel}</dd>
+                                </dl>
+                                <dl>
+                                    <dt>Ship to</dt>
+                                    <dd>{this.state.product.whatUse}</dd>
+                                </dl>
+                                <dl>
+                                    <dt>Requested by</dt>
+                                    <dd>{this.state.product.whoupload}</dd>
+                                </dl>
+                                <dl>
+                                    <dt>Customer</dt>
+                                    <dd>{this.state.product.customer}</dd>
+                                </dl>
+                                <dl>
+                                    <dt>Tgt price</dt>
+                                    <dd>{this.state.product.tgtPrice}</dd>
+                                </dl>
+                                <dl>
+                                    <dt>Description</dt>
+                                    <dd>{this.state.product.remark}</dd>
+                                </dl>
+                                <Link to = {`/edit/${this.state.key}`} class = "btn btn-success">Edit</Link>
+                                &nbsp;&nbsp;&nbsp;<button onClick={this.delete.bind(this, this.state.key,this.state.whouploadId, this.state.stallId )} class="btn btn-danger">Delete</button>
+                                <dl></dl>
+                            </div>
+                        </card>
                     </div>
-                    &nbsp;
-                    <div>
-                        <img src = {this.state.product.image} height = "200" width = "200"/>
-                    </div>
-                    <div class= "panel panel-default">
-                        <h3 class="panel-title">{this.state.product.whatPN}</h3>
-                    </div>
-                    <div class = "panel-body">
-                        <dl>
-                            <dd>Qty:  {this.state.product.whatQty}</dd>
-                        </dl>
-                        <dl>
-                            <dt>Model</dt>
-                            <dd>{this.state.product.whatModel}</dd>
-                        </dl>
-                        <dl>
-                            <dt>Ship to</dt>
-                            <dd>{this.state.product.whatUse}</dd>
-                        </dl>
-                        <dl>
-                            <dt>Requested by</dt>
-                            <dd>{this.state.product.whoupload}</dd>
-                        </dl>
-                        <dl>
-                            <dt>Customer</dt>
-                            <dd>{this.state.product.customer}</dd>
-                        </dl>
-                        <dl>
-                            <dt>Tgt price</dt>
-                            <dd>{this.state.product.tgtPrice}</dd>
-                        </dl>
-                        <dl>
-                            <dt>Description</dt>
-                            <dd>{this.state.product.remark}</dd>
-                        </dl>
-                        <Link to = {`/edit/${this.state.key}`} class = "btn btn-success">Edit</Link>
-                        &nbsp;&nbsp;&nbsp;<button onClick={this.delete.bind(this, this.state.key,this.state.whouploadId, this.state.stallId )} class="btn btn-danger">Delete</button>
-                        <dl></dl>
-                    </div>
-        </card>
-    </div>
-  </div>
+                </div>
   
-  <div class="column">
-            {this.state.pictures.map (picture =>
-                <td><img src={picture.image}width="180px" height="180" alt=""></img></td>
-            )}
+                <div class="column">
+                    {this.state.pictures.map (picture =>
+                        <td><img src={picture.image}width="180px" height="180" alt=""></img></td>
+                    )}
 
-            <div className="upload-data">
-                &nbsp;&nbsp;
-                <input type="file" onChange={this.handleChanges}/>    
-            </div>
-            
-            
-            <div class = "msg">
-                <dt>Messages</dt>
-                <div id="just-line-break">{this.state.message.messages}</div>
-            </div>
-  </div>
-</div>
+                    <div className="upload-data">
+                        &nbsp;&nbsp;
+                        <input type="file" onChange={this.handleChanges}/>    
+                    </div>
+                    <br></br>
+                    <div>
+                        <dt>&nbsp;&nbsp;&nbsp;Messages</dt>
+                        
+                        <div id="scrollId">
+                        <div class = "msg">
+                        <div id="just-line-break">{this.state.message.messages}</div>
+                        </div>
+                        </div>
+                    </div>
 
-          
+                    <dt>&nbsp;&nbsp;&nbsp;{this.state.poExisted}</dt>
+                    <br></br>
+                    <div className="upload-data">
+                        &nbsp;&nbsp;
+                        <input type="file" onChange={this.uploadPO}/>
+                        <div>
+                            <embed src={this.state.urlPO} width="200" height="100" type="application/pdf"></embed>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )}
-}
+    }
     export default ShowProduct
