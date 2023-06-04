1314ff86ee62edfc009648818ebf5b45b1c7cf6e
kentankengboon <kentankengboon@gmail.com>
2023-06-04 22:23:40 +0800

    add env port at index.js

--- src/index.js
+++ src/index.js
@@ -15,13 +15,13 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import './node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Login from './Components/Login';
 
-/*
+
 const express = require('express');
 const app = express();
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
-*/
+
 //const port = process.env.PORT || 3000
 //ReactDOM.render(<React.StrictMode><App /></React.StrictMode>,document.getElementById('root'));
 ReactDOM.render(<BrowserRouter>
