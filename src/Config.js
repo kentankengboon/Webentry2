b540935e849e373d8302cab0d4ddbf2e64968d1a
kentankengboon <kentankengboon@gmail.com>
2023-06-04 22:02:19 +0800

    add port setting to config file

--- src/Config.js
+++ src/Config.js
@@ -18,3 +18,14 @@ firebase.firestore().settings(settings);
 //firebase.auth().settings(settings);
 export default firebase;
 // Initialize Firebase
+
+
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;  // Use the PORT environment variable or fallback to port 3000
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
