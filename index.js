467f2cc15fea7afe31c2f8149263292d00236e80
kentankengboon <kentankengboon@gmail.com>
2023-06-11 00:59:30 +0800

    list at dotenv

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //const port = process.env.Port || 3000;
 //console.log(port)
 //import express from '../node_modules/express';
-const express = require('express');
-const app = express();
-app.listen(process.env.PORT || 3000, ()=>{
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+//const express = require('express');
+//const app = express();
+//app.listen(process.env.PORT || 3000, ()=>{
+//  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+//});
 
