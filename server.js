6b90e38e2c4b3cd7ed7626d3a137d343e8eec8d4
kentankengboon <kentankengboon@gmail.com>
2023-06-12 00:31:46 +0800

    server.js added

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+
+
+
 //const port = process.env.Port || 3000;
 //console.log(port)
 //import express from '../node_modules/express';
@@ -6,4 +9,20 @@
 //app.listen(process.env.PORT || 3000, ()=>{
 //  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 //});
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve the static files
+app.use(express.static('build'));
+
+// Handle other routes or API endpoints
+// ...
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 
