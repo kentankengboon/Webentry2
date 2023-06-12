3df40de408be04c7d834d78f1a10e8c8d7327ae5
kentankengboon <kentankengboon@gmail.com>
2023-06-12 10:34:35 +0800

    node server on top

--- server.js
+++ server.js
@@ -12,7 +12,7 @@
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Serve the static files
 app.use(express.static('build'));
